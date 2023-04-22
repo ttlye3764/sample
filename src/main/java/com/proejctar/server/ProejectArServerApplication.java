2321852c922042a6dde1b428388b67fc23215e02
jaeholee <ttlye5@kyobobook.com>
2023-04-22 18:13:03 +0900

    first commit

---
+++ src/main/java/com/proejctar/server/ProejectArServerApplication.java
@@ -0,0 +1,13 @@
+package com.proejctar.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProejectArServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProejectArServerApplication.class, args);
+    }
+
+}
